8ebf825d1cb997789295de2d82c7cadf09b8bb2d
ruslansfrov236 <ruslansfrov236@gmail.com>
2023-11-14 22:07:21 +0400

    2

---
+++ Microservices-Example/OrderAPI/Models/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using OrderAPI.Models.Entities.Enum;
+
+namespace OrderAPI.Models.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid BuyerId { get; set; }
+        public Guid OrderId { get; set; }   
+        public long TotalPrice { get; set; }
+        public ICollection <OrderItem> OrderItems { get; set; }
+        public OrderStatus OrderStatu { get; set; }    
+        public DateTime CreatedTime { get; set; }   
+        public Order Orders { get; set; }    
+    }
+}
